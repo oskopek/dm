2e596d2a078e44275a490dd3b585bff5ac6d040f
Ondrej Skopek <skopekondrej@gmail.com>
2017-11-04 12:54:05 +0100

    Dtype trick + pep8

--- dm-project-2/sgd.py
+++ dm-project-2/sgd.py
@@ -11,18 +11,20 @@ FEATURE_IDS = range(features)
 
 
 # Polynomial kernel with d = 2, c = 0 for (x^T * y + c)^d
-def transform(X, dtype=np.float32):
+# Dtype trick: float16 for evaluation, float32 for training (memory limit).
+def transform(X, dtype=np.float16):
     # Make sure this function works for both 1D and 2D NumPy arrays.
     if len(X.shape) == 1:
         X = np.reshape(X, (1, -1))
 
     n_features = X.shape[1]
     sqrt2 = np.sqrt(2)
-    normalizer = np.ones((n_features, n_features)) - np.eye(n_features) * (1 - (1 / sqrt2))
+    normalizer = np.ones((n_features, n_features)) - \
+        np.eye(n_features) * (1 - (1 / sqrt2))
     Xnew = np.zeros((X.shape[0], n_features**2), dtype=dtype)
     for i, row in enumerate(X):
-        rrT = sqrt2 * np.outer(row, row) #  row * row^T
-        rrT *= normalizer #  element by element, divides diagonal by sqrt(2)
+        rrT = sqrt2 * np.outer(row, row)  # row * row^T
+        rrT *= normalizer  # element by element, divides diagonal by sqrt(2)
         Xnew[i, :] = rrT.flat
     return Xnew
 
@@ -31,18 +33,18 @@ def transform(X, dtype=np.float32):
 def ocp_svm_adam(X, y, lambada=1, beta1=0.9, beta2=0.999):
     EPS = 0.000001
 
-    m_t_prev = 0 # last 1st moment
-    v_t_prev = 0 # last 2nd moment
+    m_t_prev = 0  # last 1st moment
+    v_t_prev = 0  # last 2nd moment
     w = np.zeros((features**2,))
     for t, row in enumerate(X):
         row = transform(row, dtype=np.float32)
         row = np.reshape(row, (features**2))
-        lr_t = lambada * np.sqrt(1 - beta2**(t+1)) / (1 - beta1**(t+1))
+        lr_t = lambada * np.sqrt(1 - beta2**(t + 1)) / (1 - beta1**(t + 1))
         g_t = -y[t] * row
         m_t = beta1 * m_t_prev + (1 - beta1) * g_t
         v_t = beta2 * v_t_prev + (1 - beta2) * (g_t * g_t)
-        m_hat_t = m_t / (1 - beta1**(t+1))
-        v_hat_t = v_t / (1 - beta2**(t+1))
+        m_hat_t = m_t / (1 - beta1**(t + 1))
+        v_hat_t = v_t / (1 - beta2**(t + 1))
         if y[t] * np.dot(row, w) < 1:
             w -= lr_t * m_hat_t / (np.sqrt(v_hat_t) + EPS)
 
@@ -61,9 +63,9 @@ def ocp_svm_sgd(X, y, lambada=1):
     for t, row in enumerate(X):
         row = transform(row, dtype=np.float32)
         row = np.reshape(row, (features**2))
-        eta = eta_mult * 1 / np.sqrt(t+1)
+        eta = eta_mult * 1 / np.sqrt(t + 1)
         if y[t] * np.dot(row, w) < 1:
-            #w = w - eta * (-y[t] * row)
+            # w = w - eta * (-y[t] * row)
             w += eta * y[t] * row
             w *= min(1, inv_lambda / np.linalg.norm(w))
     return w
@@ -77,7 +79,7 @@ def mapper(key, value):
     value = np.array([map(np.float32, row.split()) for row in value])
     X = value[:, 1:]
     y = value[:, 0]
-    #  yield 0, ocp_svm_sgd(X, y)
+    # yield 0, ocp_svm_sgd(X, y)
     yield 0, ocp_svm_adam(X, y)
 
 
@@ -88,4 +90,3 @@ def reducer(key, values):
     # values: list of all value for that key
     # Note that we do *not* output a (key, value) pair here.
     yield np.mean(values, axis=0)
-
