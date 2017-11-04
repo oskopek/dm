acc2f5a577bddc73674b2e0a5ffdd347d2500738
Ondrej Skopek <skopekondrej@gmail.com>
2017-11-04 11:44:53 +0100

    float16

--- dm-project-2/sgd.py
+++ dm-project-2/sgd.py
@@ -4,26 +4,29 @@ import random
 np.random.seed(42)
 random.seed(42)
 
-FEATURE_IDS = np.random.choice(range(400), 200, replace=False)
+FEATURE_IDS = np.random.choice(range(400), 320, replace=False)
 
 def transform(X):
     # Make sure this function works for both 1D and 2D NumPy arrays.
-    print 'x shape:', X.shape
     X1 = X[:, FEATURE_IDS]
-    print 'x1 shape:', X1.shape
-    X2 = np.zeros((X1.shape[0], X1.shape[1]**2))
-    print 'x2 shape:', X2.shape
+    X2 = np.zeros((X1.shape[0], X1.shape[1]**2), dtype=np.float16)
+    sqrt2 = np.sqrt(2)
     for i, row in enumerate(X1):
-        X2[i, :] = np.outer(row, row).flat
-    return np.concatenate((X, X2), axis=1)
+        Xn = sqrt2 * np.outer(row, row)
+        Xn *= np.ones((len(row), len(row))) - np.eye(len(row)) * (1 - 1/sqrt2)
+        X2[i, :] = Xn.flat
+    #return np.concatenate((X, X2), axis=1)
+    return X2
 
 def ocp_svm(X, y, lambada=10, beta1=0.9, beta2=0.999):
+    print("Training batch")
     EPS = 0.000001
 
     m_t_prev = 0 # last 1st moment
     v_t_prev = 0 # last 2nd moment
     w = np.zeros((X.shape[1],))
     for t, row in enumerate(X):
+        row = row.astype(np.float32)
         lr_t = lambada * np.sqrt(1 - beta2**(t+1)) / (1 - beta1**(t+1))
         g_t = -y[t] * row
         m_t = beta1 * m_t_prev + (1 - beta1) * g_t
@@ -42,7 +45,7 @@ def mapper(key, value):
     # key: None
     # value: one line of input file
     np.random.shuffle(value)
-    value = np.array([map(float, row.split()) for row in value])
+    value = np.array([map(np.float32, row.split()) for row in value])
     X = value[:, 1:]
     y = value[:, 0]
     X = transform(X)
