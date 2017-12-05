6e72fae4a8880283e55334320dcc2b485a342b92
Ondrej Skopek <skopekondrej@gmail.com>
2017-12-05 18:15:54 +0100

    Fixed flake8 and added comments.

--- dm-project-3/kmeans.py
+++ dm-project-3/kmeans.py
@@ -3,33 +3,42 @@ import numpy as np
 import random
 from scipy.spatial.distance import cdist
 
+# The number of preprocessed features in the input.
 NUM_FEATURES = 250
 
+# Set the random seed
 random.seed(42)
 np.random.seed(42)
 
+
+# Calculate the squared distances, rows represent distances from centers,
+# columns distances from points in xs.
+def sqdist(centers, xs):
+    return cdist(centers, xs, metric='sqeuclidian')
+
+
+# Sample n_samples points from xs according to the distribution p,
+# without replacement.
 def sample_from_points(xs, p, n_samples):
     assert(n_samples <= xs.shape[0])
     return xs[np.random.choice(xs.shape[0], n_samples, replace=False, p=p)]
 
-def d2sample(xs, centroids, n_clusters=200):
-    distances = cdist(centroids, xs, metric='sqeuclidean')
-    min_distances = np.min(distances, axis=0)
+
+# Sample n_samples points from xs based on the inverse squared distance
+# to centers.
+def d2sample(xs, centers, n_clusters=200):
+    distances = sqdist(centers, xs)
+    min_distances = np.min(distances, axis=0)  # from points to centers
     probs = min_distances / np.sum(min_distances)
     return sample_from_points(xs, probs, n_clusters)
 
-def kpp_sample_iter(xs, distances, iteration):
-    min_distances = np.min(distances[:iteration], axis=0)
-    probs = min_distances / np.sum(min_distances)
-    new_point = sample_from_points(xs, probs, 1)
-    distances[iteration,:] = cdist(new_point, xs, metric='sqeuclidean')
-    return new_point
 
-def d3sample(xs, centroids, n_clusters=200):
+# Implements coreset construction by iterative sampling.
+# Paper: Algorithm 1: TODO
+def d3sample(xs, centers, n_clusters=200):
     n_points = xs.shape[0]
-    # alpha = np.log2(n_clusters)+1
     alpha = 16 * (np.log2(n_clusters) + 2)
-    distances = cdist(centroids, xs, metric='sqeuclidean')
+    distances = sqdist(centers, xs)
     min_distances = np.min(distances, axis=0)
     c_phi = np.sum(min_distances) / n_points
     B_is = closest_centroid_indices = np.argmin(distances, axis=0)
@@ -41,74 +50,92 @@ def d3sample(xs, centroids, n_clusters=200):
     B_i_counts[unique] = counts
     B_i_lens = B_i_counts[B_is]
     sum_of_dists_in_cluster = np.zeros(n_clusters)
-    sum_of_dists_in_cluster = np.vectorize(lambda i: np.sum(min_distances[B_is==i]))(range(n_clusters))
+    sum_of_dists_in_cluster = np.vectorize(
+        lambda i: np.sum(min_distances[B_is == i]))(range(n_clusters))
 
-    probs  = alpha*min_distances/c_phi + \
-             2*alpha*sum_of_dists_in_cluster[B_is] / (B_i_lens * c_phi) + \
-             4 * n_points/B_i_lens
+    probs = alpha * min_distances / c_phi + \
+        2 * alpha * sum_of_dists_in_cluster[B_is] / (B_i_lens * c_phi) + \
+        4 * n_points / B_i_lens
 
     probs = probs / np.sum(probs)
     return sample_from_points(xs, probs, n_clusters)
 
-def kmeans_pp(xs, n_clusters):
-    n_points = xs.shape[0]
-    probs = np.ones(n_points)/n_points
-    centroids = np.zeros((n_clusters, NUM_FEATURES))
-    centroids[0,:] = sample_from_points(xs, probs, 1)
-    distances = np.zeros((n_clusters, n_points))
-    distances[0,:] = cdist([centroids[0,:]], xs, metric='sqeuclidean')
-    for k in range(1, n_clusters):
-        centroids[k,:] = kpp_sample_iter(xs, distances, k)
-    return centroids
 
-def kmeans_mem(xs, initial_centroids=None, n_clusters=200, n_iterations=50, early_stopping=0.2):
+# Implements kmeans++.
+def kmeans_pp(xs, n_clusters):
+    # Sample a new point based on the inverse squared distance to centers,
+    # iteratively; uses distances as a matrix that it fills in top to bottom.
+    def kpp_sample_iter(xs, distances, iteration):
+        min_distances = np.min(distances[:iteration], axis=0)
+        probs = min_distances / np.sum(min_distances)
+        new_point = sample_from_points(xs, probs, 1)
+        distances[iteration, :] = sqdist(new_point, xs)
+        return new_point
+
+    # Initial distribution.
     n_points = xs.shape[0]
-    if initial_centroids is None:
-        centroids = kmeans_pp(xs, n_clusters)
-        print('kpp')
+    probs = np.ones(n_points) / n_points
+
+    centers = np.zeros((n_clusters, NUM_FEATURES))
+    distances = np.zeros((n_clusters, n_points))
+    centers[0] = sample_from_points(xs, probs, 1)
+    distances[0] = sqdist([centers[0]], xs)
+    for k in range(1, n_clusters):
+        centers[k] = kpp_sample_iter(xs, distances, k)
+    return centers
+
+
+# Implements kmeans with all points and distances in memory.
+def kmeans_mem(xs, initial_centers=None, n_clusters=200,
+               n_iterations=50, early_stopping=0.2):
+    # Run kmeans++ if initial_centers were not provided.
+    if initial_centers is None:
+        centers = kmeans_pp(xs, n_clusters)
+        print('kmeans++:\tend')
     else:
-        centroids = initial_centroids
-    centroids_old = np.zeros_like(centroids)
+        centers = initial_centers
+
+    centers_old = np.zeros_like(centers)
     for iteration in range(n_iterations):
-        diff = np.linalg.norm(centroids-centroids_old)
+        # Early stopping based on the norm of the change in centers.
+        diff = np.linalg.norm(centers - centers_old)
         if diff < early_stopping:
             break
-        distances = cdist(centroids, xs, metric='sqeuclidean')
+        distances = sqdist(centers, xs)
         idxs = np.argmin(distances, axis=0)
-        assert idxs.shape[0] == n_points
-        centroids_old = np.copy(centroids)
+        assert idxs.shape[0] == xs.shape[0]
+        centers_old = np.copy(centers)
         for i in range(n_clusters):
             idxs_i = idxs == i
             if idxs_i.any():
-                centroids[i] = np.mean(xs[idxs_i], axis=0)
+                centers[i] = np.mean(xs[idxs_i], axis=0)
 
-    return centroids
+    return centers
 
+
+# Mapper: runs d3sample twice on uniformly sampled centers and yields them.
 def mapper(key, value):
     # key: None
     # value: Numpy array of the points.
     n_clusters = 3000
     n_points = value.shape[0]
     if (n_clusters < n_points):
-        uniform_probs = np.ones(value.shape[0]) / value.shape[0]
-        centers = sample_from_points(value, p=uniform_probs, n_samples=n_clusters)
+        uniform_probs = np.ones(n_points) / n_points
+        centers = sample_from_points(
+            value, p=uniform_probs, n_samples=n_clusters)
         for i in range(2):
             centers = d3sample(value, centers, n_clusters=n_clusters)
     else:
         centers = value
-    print('map')
+    print('mapper:\tend')
     yield 0, centers
 
-# Not used
-def eval_k_means(xs, centers):
-    distances = cdist(centers, xs, metric='sqeuclidean')
-    min_distances = np.min(distances, axis=0)
-    return np.mean(min_distances)
 
+# Reducer: runs kmeans_mem on the centers provided (includes KMeans++).
 def reducer(key, values):
     # key: key from mapper used to aggregate (constant)
     # values: list of cluster centers.
-    batch_centroids = np.reshape(values, (-1, NUM_FEATURES))
-    centers = kmeans_mem(batch_centroids)
-    print('ex')
+    batch_centers = np.reshape(values, (-1, NUM_FEATURES))
+    centers = kmeans_mem(batch_centers)
+    print('reducer:\tend')
     yield centers
