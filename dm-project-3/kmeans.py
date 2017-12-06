18c7d6a81f04ad03e363929a2775e05a6ebc7405
Lukas Jendele <lukas.jendele@gmail.com>
2017-12-06 12:47:07 +0100

    parameter tweaking

--- dm-project-3/kmeans.py
+++ dm-project-3/kmeans.py
@@ -8,10 +8,13 @@ NUM_FEATURES = 250
 random.seed(42)
 np.random.seed(42)
 
-def sample_from_points(xs, p, n_samples):
+def sample_from_points(xs, p, n_samples, output_weights=False):
     assert(n_samples <= xs.shape[0])
     idx = np.random.choice(xs.shape[0], n_samples, replace=False, p=p)
-    return xs[idx], p[idx]
+    if output_weights:
+        return xs[idx], 1/p[idx]
+    else:
+        return xs[idx]
 
 def d2sample(xs, centroids, n_clusters=200):
     distances = cdist(centroids, xs, metric='sqeuclidean')
@@ -49,33 +52,34 @@ def d3sample(xs, centroids, n_clusters=200):
              4 * n_points/B_i_lens
 
     probs = probs / np.sum(probs)
-    return sample_from_points(xs, probs, n_clusters)
+    return sample_from_points(xs, probs, n_clusters, True)
 
-def kmeans_pp(xs, n_clusters):
+def kmeans_pp(xs, n_clusters, weights):
     n_points = xs.shape[0]
     probs = np.ones(n_points)/n_points
     centroids = np.zeros((n_clusters, NUM_FEATURES))
     centroids[0,:] = sample_from_points(xs, probs, 1)
     distances = np.zeros((n_clusters, n_points))
-    distances[0,:] = cdist([centroids[0,:]], xs, metric='sqeuclidean')
+    distances[0,:] = cdist([centroids[0,:]], xs, metric='sqeuclidean') * weights
     for k in range(1, n_clusters):
         centroids[k,:] = kpp_sample_iter(xs, distances, k)
     return centroids
 
-def kmeans_mem(xs, initial_centroids=None, n_clusters=200, n_iterations=50, early_stopping=0.2):
+def kmeans_mem(xs, initial_centroids=None, n_clusters=200, n_iterations=50, early_stopping=0.2, weights=1):
     n_points = xs.shape[0]
     if initial_centroids is None:
-        centroids = kmeans_pp(xs, n_clusters)
+        centroids = kmeans_pp(xs, n_clusters, weights)
         print('kpp')
     else:
         centroids = initial_centroids
+
     centroids_old = np.zeros_like(centroids)
     for iteration in range(n_iterations):
         diff = np.linalg.norm(centroids-centroids_old)
         print 'it', iteration, ' diff: ', diff
         if diff < early_stopping:
             break
-        distances = cdist(centroids, xs, metric='sqeuclidean')
+        distances = cdist(centroids, xs, metric='sqeuclidean') * weights
         idxs = np.argmin(distances, axis=0)
         assert idxs.shape[0] == n_points
         centroids_old = np.copy(centroids)
@@ -89,17 +93,13 @@ def kmeans_mem(xs, initial_centroids=None, n_clusters=200, n_iterations=50, earl
 def mapper(key, value):
     # key: None
     # value: Numpy array of the points.
-    n_clusters = 3000
     n_points = value.shape[0]
-    if (n_clusters < n_points):
-        uniform_probs = np.ones(value.shape[0]) / value.shape[0]
-        centers = sample_from_points(value, p=uniform_probs, n_samples=n_clusters)
-        for i in range(2):
-            centers = d3sample(value, centers, n_clusters=n_clusters)
-    else:
-        centers = value
+    n_clusters = min(n_points-1, 3000)
+    uniform_probs = np.ones(value.shape[0]) / value.shape[0]
+    centers = sample_from_points(value, p=uniform_probs, n_samples=n_clusters)
+    centers, weights = d3sample(value, centers, n_clusters=n_clusters)
     print('map')
-    yield 0, (centers, "separator", value)
+    yield 0, (centers, weights, "separator", value)
 
 # Not used
 def eval_k_means(xs, centers):
@@ -112,13 +112,12 @@ def reducer(key, values):
     # values: list of cluster centers.
     batch_centroids = np.asarray([value[0] for value in values])
     batch_centroids = np.reshape(batch_centroids, (-1, NUM_FEATURES))
+    centroids_weights = np.asarray([value[1] for value in values])
+    centroids_weights = np.reshape(centroids_weights, (-1))
+    all_pts = np.asarray([value[3] for value in values])
+    all_pts = np.reshape(all_pts, (-1, NUM_FEATURES))
 
-    all_points = np.asarray([value[2] for value in values])
-    all_points = np.reshape(all_points, (-1, NUM_FEATURES))
-
-
-    centers = kmeans_mem(batch_centroids)
-    print('first kmeans')
-    res_centers = kmeans_mem(all_points, initial_centroids=centers, early_stopping=0.5)
+    centers = kmeans_mem(batch_centroids, weights=centroids_weights, early_stopping=0.09)
+    centers = kmeans_mem(all_pts, initial_centroids=centers, early_stopping=0.5)
     print('ex')
     yield centers
