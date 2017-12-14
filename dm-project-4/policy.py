c86cbd50c9014cb9cbc88ff18fbdfe61f85da1f5
Ondrej Skopek <skopekondrej@gmail.com>
2017-12-14 20:11:33 +0100

    Fixed Hybrid UCB?

--- dm-project-4/policy.py
+++ dm-project-4/policy.py
@@ -3,15 +3,15 @@ import numpy as np
 USER_FEATURES = 6
 ARTICLE_FEATURES = 6
 
-DELTA = 0.05
+DELTA = 0.2
 ALPHA = 1 + np.sqrt(np.log(2/DELTA)/2)
 print(ALPHA)
 
 articles = None
 
-A0 = np.eye(ARTICLE_FEATURES)
-A0inv = np.eye(ARTICLE_FEATURES)
-b0 = np.zeros(ARTICLE_FEATURES)
+A0 = np.eye(USER_FEATURES)
+A0inv = np.eye(USER_FEATURES)
+b0 = np.zeros(USER_FEATURES)
 
 A = dict()
 Ainvs = dict()
@@ -28,10 +28,10 @@ def set_articles(articles_local):
     # articles - dictionary of (about 80) article id -> features (of len 6)
     for article_id, article in articles.iteritems():
         articles[article_id] = np.asarray(article)
-        A[article_id] = np.eye(USER_FEATURES, USER_FEATURES)
-        Ainvs[article_id] = np.eye(USER_FEATURES, USER_FEATURES)
-        B[article_id] = np.zeros((USER_FEATURES, ARTICLE_FEATURES))
-        b[article_id] = np.zeros(USER_FEATURES)
+        A[article_id] = np.eye(ARTICLE_FEATURES, ARTICLE_FEATURES)
+        Ainvs[article_id] = np.eye(ARTICLE_FEATURES, ARTICLE_FEATURES)
+        B[article_id] = np.zeros((ARTICLE_FEATURES, USER_FEATURES))
+        b[article_id] = np.zeros(ARTICLE_FEATURES)
 
 
 def update(reward):
@@ -42,18 +42,20 @@ def update(reward):
     article_id = last_chosen_article_id
     zt = last_zt
 
-    x = articles[article_id]
-    A0 += np.dot(B[article_id], np.dot(Ainvs[article_id], B[article_id]))
-    b0 += np.dot(B[article_id], np.dot(Ainvs[article_id], b[article_id]))
+    dott = B[article_id].T.dot(Ainvs[article_id])
+    A0 += dott.dot(B[article_id])
+    b0 += dott.dot(b[article_id])
 
+    x = articles[article_id]
     A[article_id] += np.outer(x, x)
     Ainvs[article_id] = np.linalg.inv(A[article_id])
     B[article_id] += np.outer(x, zt)
     b[article_id] += reward * x
 
-    A0 += np.outer(zt, zt) - np.dot(B[article_id], np.dot(Ainvs[article_id], B[article_id]))
+    dott2 = B[article_id].T.dot(Ainvs[article_id])
+    A0 += np.outer(zt, zt) - dott2.dot(B[article_id])
     A0inv = np.linalg.inv(A0)
-    b0 += reward * zt - np.dot(B[article_id], np.dot(Ainvs[article_id], b[article_id]))
+    b0 += reward * zt - dott.dot(b[article_id])
 
     last_zt = None
     last_chosen_article_id = -1
@@ -71,13 +73,13 @@ def recommend(time, user_features, choices):
     for article_id in choices:
         Ainv = Ainvs[article_id]
         x = articles[article_id]
-        theta = np.dot(Ainv, (b[article_id] - np.dot(B[article_id], beta)))
+        theta = Ainv.dot(b[article_id] - B[article_id].dot(beta))
 
-        middle_vector = np.dot(A0inv, np.dot(B[article_id].T, np.dot(Ainv, x)))
-        s = np.dot(zt, np.dot(A0inv, zt)) - 2 * np.dot(zt, middle_vector) + np.dot(x, np.dot(Ainv, x))\
-            + np.dot(x, np.dot(Ainv, np.dot(B[article_id], middle_vector)))
+        middle_vector = A0inv.dot(B[article_id].T.dot(Ainv.dot(x)))
+        s = zt.dot(A0inv.dot(zt)) - 2 * zt.dot(middle_vector) + x.dot(Ainv.dot(x))\
+            + x.dot(Ainv.dot(B[article_id].dot(middle_vector)))
 
-        p = np.dot(zt, beta) + np.dot(x, theta) + ALPHA * np.sqrt(s)
+        p = zt.dot(beta) + x.dot(theta) + ALPHA * np.sqrt(s)
         if p > p_max:
             p_max = p
             p_argmax = article_id
